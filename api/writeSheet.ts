e550973e260108560400ea6ca788ca149d6a2855
yukiundermount <yuki@under-mount-industry.com>
2026-01-05 02:52:14 +0900

    Update writeSheet.ts

--- api/writeSheet.ts
+++ api/writeSheet.ts
@@ -1,10 +1,7 @@
-import type { VercelRequest, VercelResponse } from "vercel";
 import { google } from "googleapis";
 
-export default async function handler(
-  req: VercelRequest,
-  res: VercelResponse
-) {
+export default async function handler(req: any, res: any) {
+
   if (req.method !== "POST") {
     return res.status(405).json({ error: "Method not allowed" });
   }
