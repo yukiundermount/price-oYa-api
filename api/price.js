7191785bf0ce3acbb679a83e5718c47e004715d3
yukiundermount <yuki@under-mount-industry.com>
2026-01-06 18:34:01 +0900

    Update price.js

--- api/price.js
+++ api/price.js
@@ -1,5 +1,5 @@
 import OpenAI from "openai";
-import writeSheet from "./writeSheet.js";
+import { writeSheet } from "./writeSheet.js";
 
 const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
 
